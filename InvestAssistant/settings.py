ed811abc8f18d0e7dc8c8b35b30871d661e0e258
Kalin Krumov <kaweeo@gmail.com>
2024-11-25 18:32:19 +0200

    chor: install and register apps

--- InvestAssistant/settings.py
+++ InvestAssistant/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # MY_APPS
+    'InvestAssistant.investments',
+    'InvestAssistant.transactions',
+    'InvestAssistant.common'
+    'InvestAssistant.accounts',
 ]
 
 MIDDLEWARE = [
