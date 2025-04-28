eb79a27553cd1ef8dd2bf0505065dbb617471695
Kalin Krumov <kaweeo@gmail.com>
2025-04-28 15:41:35 +0300

    fix: Setup currency processor with contxt_processor

--- InvestAssistant/common/context_processors.py
+++ InvestAssistant/common/context_processors.py
@@ -5,6 +5,7 @@ def currency_processor(request):
     Context processor that adds the DEFAULT_CURRENCY to the context
     of all templates.
     """
+    
     return {
         'DEFAULT_CURRENCY': settings.DEFAULT_CURRENCY,
     } 
