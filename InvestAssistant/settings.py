0a4d8257d5ffd5128d061ba8d033b28f7e6baa80
Kalin Krumov <kaweeo@gmail.com>
2025-06-04 16:34:27 +0300

    fix: explicitly added localhost in ALLOWED_HOSTS

--- InvestAssistant/settings.py
+++ InvestAssistant/settings.py
@@ -22,9 +22,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY'
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
